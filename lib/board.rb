e27b25eb53fdeb48bcbf20af859df8eb0702fcfa
Travis <tzito93@gmail.com>
2022-04-13 21:17:36 -0500

    test for piece placement with visualization in lib/main.rb

--- lib/board.rb
+++ lib/board.rb
@@ -3,16 +3,42 @@
 require 'colorize'
 
 class Board
+  attr_accessor :rows
+
   def initialize
-    @rows = Array.new(6) { Array.new(7, '⬤') }
+    @rows = Array.new(6) { Array.new(7, nil) }
   end
 
   def render
     @rows.each do |row|
       row.each do |circle|
-        print circle.colorize(:color => :white) + ' '
+        if circle.nil?
+          print '⬤'.colorize(:white) + ' '
+        else
+          print circle + ' '
+        end
       end
       puts
     end
+  end
+
+  def [](pos)
+    row, col = pos
+    @rows[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    @rows[row][col] = value
+  end
+
+  def place_piece(piece, col)
+    row = rows.length - 1 # starts at the 'bottom' of the board
+    until self[[row, col]].nil?
+      return 'This column is full' if row == -1
+
+      row -= 1
+    end
+    self[[row, col]] = piece      
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,5 +3,62 @@
 require_relative '../lib/board'
 
 describe Board do
+  describe '#place_piece' do
+    subject(:board) { described_class.new }    
 
+    context 'when the column is empty' do
+      before do
+        board.instance_variable_set(:@rows, [
+          [nil, nil, nil],
+          [nil, nil, nil],
+          [nil, nil, nil]
+        ])
+      end
+
+      it 'piece goes to the bottom of the column' do
+        expected_outcome = [
+          [nil, nil, nil],
+          [nil, nil, nil],
+          [:X, nil, nil]
+        ]
+        board.place_piece(:X, 0)
+        expect(board.rows).to eq(expected_outcome)
+      end
+    end
+
+    context 'when the column has some pieces' do
+      before do
+        board.instance_variable_set(:@rows, [
+          [nil, nil, nil],
+          [nil, :X, nil],
+          [nil, :X, nil]
+        ])
+      end
+      
+      it 'piece goes to the next empty space in the column' do
+        expected_outcome = [
+          [nil, :X, nil],
+          [nil, :X, nil],
+          [nil, :X, nil]
+        ]        
+        board.place_piece(:X, 1)
+        expect(board.rows).to eq(expected_outcome)
+      end
+    end
+
+    context 'when the column is full' do
+      before do
+        board.instance_variable_set(:@rows, [
+          [nil, :X, nil],
+          [nil, :X, nil],
+          [nil, :X, nil]
+        ])
+      end
+      
+      it 'notifies that column is full' do        
+        error_msg = board.place_piece(:X, 1)
+        expect(error_msg).to eq('This column is full')
+      end
+    end
+  end
 end
